351ecf426c4819950979d342a5d8c55cf252dc1c
jace35 <Jingram12@fingerlakes.edu>
2024-09-11 18:34:20 +0000

    work

---
+++ CH01/initials.py
@@ -0,0 +1,61 @@
+# FILE NAME: initials.py
+
+# NAME: Jace Ingram
+# DATE: 9-11-2024
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+print("JJJJJJJJJ") print("IIIIIIIII")
+print("     JJ  ") print("     II  ")
+print("     JJ  ") print("     II  ")
+print(" JJ  JJ  ") print("     ")
+print("  JJ JJ  ")
+print("    JJ   ")
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+TTTTTTTT     JJJJJJJJ
+   TT            JJ
+   TT            JJ
+   TT        JJ  JJ
+   TT         JJ JJ
+   TT           JJ
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. Did you have to submit this code more than once before it passed? If so, what
+   did you have to change?
+
+
+
+
+
+'''
