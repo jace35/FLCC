c1249c4d7c1b98554fb76f909f31080828af17e5
jace35 <Jingram12@fingerlakes.edu>
2024-09-11 18:24:37 +0000

    Dave screwed it up

---
+++ CH01/rectangle.py
@@ -0,0 +1,52 @@
+# FILE NAME: rectangle.py
+
+# NAME: Jace Ingram
+# DATE: 9/11/2024
+# BRIEF DESCRIPTION:  Prints a rectangle
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+*****
+*   *
+*   *
+*****
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. This is your first "real" lab. What is one thing that you struggled with?
+commiting my code
+
+
+
+
+'''
