a41869d6d60512d98594a3922e1e3715d2aa45f8
jace35 <Jingram12@fingerlakes.edu>
2024-09-17 04:46:04 +0000

    touch up

--- CH02/input_demo.py
+++ CH02/input_demo.py
@@ -1,5 +1,5 @@
 first_name = input("what is your name?")
-print(f"Nice to meet you, {first_name}!!!")
+print(f"Nice to meet you,{first_name}!!!")
 
-age = input(f"how old are you?, {first_name}")
-print (f"you are {age} years old!!!")
+age = input(f"how old are you{first_name}?")
+print (f"you are{age} years old!!!")
