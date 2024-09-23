df5764ebff0092caddd10950888d19954d6cebd4
jace35 <Jingram12@fingerlakes.edu>
2024-09-23 17:55:27 +0000

    1234

---
+++ CH02/triangle_area.py
@@ -0,0 +1,76 @@
+# FILE NAME - triangle_area.py
+
+# NAME: Jace Ingram
+# DATE: 9-18-24
+# BRIEF DESCRIPTION:  area of triangle
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. The Sample Output has been included in this code for your convenience
+
+def main():
+    find_area()
+
+def find_area():
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+    height = int(input("Enter the height:"))
+    base = int(input("Enter the base:"))
+
+    print("The area of the triangle is", 1/2 * height * base)
+    
+
+    ########### END YER CODE ABOVE THIS LINE ###########
+main()
+
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+Enter the height: 1
+Enter the base: 1
+
+The area of the triangle is 0.5
+
+'''
+
+
+
+'''
+
+Enter the height: 8
+Enter the base: 4
+
+The area of the triangle is 16.0
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What is the flow of the program? Which line of code kicks off the process?
+
+
+
+
+
+2. What was the hardest part of this lab?
+
+
+
+
+
+'''
